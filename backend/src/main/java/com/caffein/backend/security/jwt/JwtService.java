101c43f4b5cf1713115279409d239b1349d17fff
tansinhnguyen123 <nsinh2281@gmail.com>
2026-02-19 19:00:52 +1100

    jwt service and custom filter

--- backend/src/main/java/com/caffein/backend/security/jwt/JwtService.java
+++ backend/src/main/java/com/caffein/backend/security/jwt/JwtService.java
@@ -1,10 +1,16 @@
 package com.caffein.backend.security.jwt;
 
+import java.util.Date;
+import java.util.Map;
+
 import javax.crypto.SecretKey;
+import javax.swing.Spring;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 
@@ -22,23 +28,66 @@ public class JwtService {
   private Long refreshTokenExpiration;
 
   public String generateAccessToken(String username) {
-    return "";
+    Map<String, Object> claims = Map.of(TOKEN_TYPE, "ACCESS_TOKEN");
+    return generateToken(username, claims, accessTokenExpiration);
   }
 
   public String generateRefreshToken(String username) {
-    return "";
+    Map<String, Object> claims = Map.of(TOKEN_TYPE, "REFRESH_TOKEN");
+    return generateToken(username, claims, refreshTokenExpiration);
   }
 
-  public boolean isTokenValid(String token) {
-    return false;
+  private String generateToken(String username, Map<String, Object> claims, Long expiredTime) {
+    return Jwts.builder()
+        .claims(claims)
+        .subject(username)
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() * expiredTime))
+        .signWith(key)
+        .compact();
+  }
+
+  public String extractUsername(String token) {
+    return extractClaims(token).getSubject();
+  }
+
+  private Claims extractClaims(String token) {
+    try {
+      return Jwts
+          .parser()
+          .verifyWith(key)
+          .build()
+          .parseSignedClaims(token)
+          .getPayload();
+    } catch (Exception e) {
+      throw new RuntimeException("Invalid Jwt Token ", e);
+    }
+
+  }
+
+  public boolean isTokenValid(String token, String expectedUsername) {
+    String userName = extractUsername(token);
+    return userName.equals(expectedUsername) && !isTokenExpired(token);
   }
 
   private boolean isTokenExpired(String token) {
-    return false;
+    return extractClaims(token).getExpiration().before(new Date());
   }
 
   public String refreshAccessToken(String refreshToken) {
-    return "";
+
+    Claims claims = extractClaims(refreshToken);
+    if (!"REFRESH_TOKEN".equals(claims.get(TOKEN_TYPE))) {
+      throw new RuntimeException("Invalid token type");
+    }
+
+    if (isTokenExpired(refreshToken)) {
+      throw new RuntimeException("refresh token expired");
+    }
+
+    String username = claims.getSubject();
+    return generateAccessToken(username);
+
   }
 
 }
--- backend/src/main/java/com/caffein/backend/security/jwt/JwtValidator.java
+++ backend/src/main/java/com/caffein/backend/security/jwt/JwtValidator.java
@@ -3,6 +3,11 @@ package com.caffein.backend.security.jwt;
 import java.io.IOException;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,17 +16,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtValidator extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
-        @NonNull HttpServletRequest request, 
-        @NonNull HttpServletResponse response, 
-        @NonNull FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        if (request.getServletPath().contains("/api/v1/auth")){
+        if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -30,14 +41,31 @@ public class JwtValidator extends OncePerRequestFilter {
         String jwt;
         String username;
 
-        if (authHeader == null || !authHeader.contains("Bearer ")){
+        if (authHeader == null || !authHeader.contains("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
         jwt = authHeader.split(" ")[1];
-        // username = jwtService.extractUsername(jwt);
+        username = jwtService.extractUsername(jwt);
 
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (jwtService.isTokenValid(jwt, userDetails.getUsername())) {
+
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
 
     }
 
